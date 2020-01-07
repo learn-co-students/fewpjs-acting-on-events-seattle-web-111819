873aabf288f7dd2d48a372e000bb6e8bddd7c3f0
4myc <missch3n@gmail.com>
2020-01-06 16:30:11 -0800

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight() {
     let rightNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(rightNumbers, 10);
     
-    if (left > 0) {
+    if (left < 360) {
         dodger.style.left = `${left + 1}px`;
     }
 }
