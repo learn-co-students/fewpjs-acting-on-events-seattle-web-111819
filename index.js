885249b9205c4ef1b28a5b6f5b169084c7d514e5
4myc <missch3n@gmail.com>
2020-01-07 04:03:30 -0800

    Done.

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
     }
-});
-
-document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
         moveDodgerRight();
     }
